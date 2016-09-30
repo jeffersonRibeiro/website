ec2aa65343457f812e35ba9f6d709be2aacc2d77
jeffersonRibeiro <jefferson.ribeiro.contato@gmail.com>
2016-09-30 11:04:54 -0300

    modulo watchify substituido por gulp-browserify. Criado task no gulp que compila os scripts e mimifica o arquivo

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	uglifycss = require('gulp-uglifycss'),
-	concatcss = require('gulp-concat-css');
+var gulp 	   = require('gulp'),
+	sass       = require('gulp-sass'),
+	uglifycss  = require('gulp-uglifycss'),
+	concatcss  = require('gulp-concat-css'),
+	browserify = require('gulp-browserify'),
+	rename     = require('gulp-rename'),
+	uglify     = require('gulp-uglify'),
+	pump       = require('pump');
 
 
 var paths = {
 	scss: 'assets/scss/*.scss',
-	css: ['assets/css/*.css', '!assets/css/bundle.css']
+	css: ['assets/css/*.css', '!assets/css/bundle.min.css'],
+	script: ['assets/js/app.js', '!assets/js/bundle.min.js']
 }
 
 gulp.task('sass', function(){
@@ -17,16 +22,28 @@ gulp.task('sass', function(){
 
 gulp.task('concat-css', function(){
 	gulp.src(paths.css)
-	.pipe(concatcss('bundle.css'))
+	.pipe(concatcss('bundle.min.css'))
 	.pipe(uglifycss())
 	.pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('browserify', function(){
+	gulp.src(paths.script)
+	.pipe(browserify({
+		insertGlobals : true,
+        debug : !gulp.env.production
+	}))
+	.pipe(rename('bundle.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('assets/js'));
 });
 
 
 gulp.task('watch', function(){
 	gulp.watch(paths.scss, ['sass']);
 	gulp.watch(paths.css, ['concat-css']);//qualquer css alterado com exeção do bundle.css
+	gulp.watch(paths.script, ['browserify']);
 });
 
 
-gulp.task('default', ['sass', 'concat-css', 'watch']);
+gulp.task('default', ['sass', 'concat-css', 'browserify', 'watch']);
